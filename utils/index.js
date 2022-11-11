6e568dd58199cca89bf27c87676c0c30ffcb84c0
JamshedSadullaev <sadullayev98@gmail.com>
2022-11-10 21:18:30 -0500

    server

--- utils/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  withAuth: (req, res, next) => {
-    if (req.session.loggedIn) {
-      next();
-    } else {
-      res.status(500).json({ message: "Log in please " });
-    }
-  },
-};
